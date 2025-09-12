a96cd68f3e1e832d2c7d3eb6f046c744451133c5
moroz <morozovdaniel713@gmail.com>
2025-09-12 15:21:21 +0300

    feat(main): init fastapi root app - include routers for products and category - add healthcheck endpoint

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from app.routers import categories
+from app.routers import ptoducts
+
+app = FastAPI(
+    title="FastAPI ecommerce - Интеренет магазин",
+    version="0.1.0",
+)
+
+app.include_router(categories.router)
+app.include_router(ptoducts.router)
+
+
+@app.get("/")
+async def root():
+    """Корневой маршрут, подверждающий, что API работает."""
+    return {"message": "Добро пожаловать в API интернет-магазина!"}
