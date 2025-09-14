d8de99762a654f1131b84caad1b64895599eca89
moroz <morozovdaniel713@gmail.com>
2025-09-14 22:11:32 +0300

    fix(main): correct typo in include router products

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.routers import categories
-from app.routers import ptoducts
+from app.routers import products
 
 app = FastAPI(
     title="FastAPI ecommerce - Интеренет магазин",
@@ -8,7 +8,7 @@ app = FastAPI(
 )
 
 app.include_router(categories.router)
-app.include_router(ptoducts.router)
+app.include_router(products.router)
 
 
 @app.get("/")
