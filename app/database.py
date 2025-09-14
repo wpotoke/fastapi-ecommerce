fe0b950148a7ae9dedb0a0369d4fb06215eec1c0
moroz <morozovdaniel713@gmail.com>
2025-09-14 22:09:45 +0300

    feat(database): implement sync session and connect engine to it

--- app/database.py
+++ app/database.py
@@ -1,6 +1,9 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
