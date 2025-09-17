3f68499a312ba93e2382fe64e435c90142905ad0
moroz <morozovdaniel713@gmail.com>
2025-09-17 17:32:31 +0300

    feat(migrations): add ascyncronius and add user model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,5 @@
 from app.models.categories import Category
 from app.models.products import Product
+from app.models.users import User
 
-__all__ = ["Category", "Product"]
+__all__ = ["Category", "Product", "User"]
