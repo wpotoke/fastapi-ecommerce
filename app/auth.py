9cc07d135873c7b03316aabe86caa09710ac4e8c
moroz <morozovdaniel713@gmail.com>
2025-09-17 17:39:10 +0300

    feat(auth): implement check password, create access token, get current user(seller) functions with jwt encode and decode function

---
+++ app/auth.py
@@ -0,0 +1,88 @@
+# ruff: noqa: E712
+from datetime import datetime, timedelta, timezone
+from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+import jwt
+
+from app.models.users import User as UserModel
+from app.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+from app.dependencies.db import get_async_db
+
+
+# Создаём контекст для хеширования с использованием bcrypt
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/token")
+
+
+def hash_password(password: str) -> str:
+    """
+    Преобразует пароль в хеш с использованием bcrypt.
+    """
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """
+    Проверяет, соответствует ли введённый пароль сохранённому хешу.
+    """
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict):
+    """
+    Создаёт JWT с payload (sub, role, id, exp).
+    """
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(
+    token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_async_db)
+):
+    """
+    Проверяет JWT и возвращает пользователя из базы.
+    """
+    credential_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credential_exception
+    except jwt.ExpiredSignatureError as exc:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token has expired",
+            headers={"WWW-Authenticate": "Bearer"},
+        ) from exc
+    except jwt.PyJWTError as exc:
+        raise credential_exception from exc
+    result = await db.scalars(
+        select(UserModel).where(UserModel.email == email, UserModel.is_active == True)
+    )
+    user = result.first()
+    if user is None:
+        raise credential_exception
+    return user
+
+
+async def get_current_seller(current_user: UserModel = Depends(get_current_user)):
+    """
+    Проверяет, что пользователь имеет роль 'seller'.
+    """
+    if current_user.role != "seller":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Only sellers can perform this action",
+        )
+    return current_user
