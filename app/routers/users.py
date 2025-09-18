8d4c179c7329a6fbb437d78fea3d37197f6e65c1
moroz <morozovdaniel713@gmail.com>
2025-09-18 18:20:29 +0300

    feat(routers): Add update access token func by adding refresh token

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,3 +1,7 @@
+# pylint:disable=bad-exception-cause,catching-non-exception
+# ruff:noqa:E712
+import jwt
+
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -6,8 +10,13 @@ from sqlalchemy import select
 from app.models.users import User as UserModel
 from app.schemas import UserCreate, User as UserSchema
 from app.dependencies.db import get_async_db
-from app.auth import hash_password, verify_password, create_access_token
-
+from app.auth import (
+    hash_password,
+    verify_password,
+    create_access_token,
+    create_refresh_token,
+)
+from app.config import SECRET_KEY, ALGORITHM
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -30,6 +39,7 @@ async def create_user(user: UserCreate, db: AsyncSession = Depends(get_async_db)
 
     db.add(db_user)
     await db.commit()
+    await db.refresh(db_user)
     return db_user
 
 
@@ -51,6 +61,44 @@ async def login(
             detail="Incorrect email or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
+    access_token = create_access_token(
+        data={"sub": user.email, "role": user.role, "id": user.id}
+    )
+    refresh_token = create_refresh_token(
+        data={"sub": user.email, "role": user.role, "id": user.id}
+    )
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "bearer",
+    }
+
+
+@router.post("/refresh_token")
+async def update_access_token(
+    refresh_token: str, db: AsyncSession = Depends(get_async_db)
+):
+    """
+    Обновляет access_token с помощью refresh_token.
+    """
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate refresh token",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(refresh_token, SECRET_KEY, algorithms=[ALGORITHM])
+        email = payload.get("sub")
+        if email is None:
+            raise credentials_exception
+    except jwt.exceptions as e:
+        raise credentials_exception from e
+    result = await db.scalars(
+        select(UserModel).where(UserModel.email == email, UserModel.is_active == True)
+    )
+    user = result.first()
+    if user is None:
+        raise credentials_exception
     access_token = create_access_token(
         data={"sub": user.email, "role": user.role, "id": user.id}
     )
